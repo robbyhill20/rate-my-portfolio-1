b557b9b0bd8b17180d008a5a09e8fdfa5e24482a
Robby Hill <88348635+robbyhill20@users.noreply.github.com>
2022-03-07 20:58:45 -0800

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-
-const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmedia', {
   useNewUrlParser: true,
   useUnifiedTopology: true
